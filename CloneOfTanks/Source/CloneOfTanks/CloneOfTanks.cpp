b750964dcaa59af553ed215da89efc24171aee48
Claudio <claudio.atz@gmail.com>
2017-06-15 17:37:05 +0100

    Empty project

---
+++ CloneOfTanks/Source/CloneOfTanks/CloneOfTanks.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CloneOfTanks.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CloneOfTanks, "CloneOfTanks" );
---
+++ CloneOfTanks/Source/CloneOfTanks/CloneOfTanksGameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CloneOfTanks.h"
+#include "CloneOfTanksGameModeBase.h"
+
+
+
+
