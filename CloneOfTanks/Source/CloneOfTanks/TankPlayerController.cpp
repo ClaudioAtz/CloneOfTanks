4f645b6f0b5fe0f5e82ab7aeee73d5b5072b3369
Claudio <claudio.atz@gmail.com>
2017-06-19 20:13:25 +0100

    Added basic TankPlayerController implementation.

--- CloneOfTanks/Source/CloneOfTanks/TankPlayerController.cpp
+++ CloneOfTanks/Source/CloneOfTanks/TankPlayerController.cpp
@@ -3,6 +3,18 @@
 #include "CloneOfTanks.h"
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay() {
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (ControlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("Controlling tank %s"), *ControlledTank->GetName());
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("Not Controlling any tank"));
+	}
+}
+
 ATank* ATankPlayerController::GetControlledTank() const {
 	return Cast<ATank>(GetPawn());
 }
