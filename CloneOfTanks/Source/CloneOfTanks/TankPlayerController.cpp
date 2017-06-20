29674aee7ced00db01cf8208802e655743023104
Claudio <claudio.atz@gmail.com>
2017-06-20 12:09:50 +0100

    Added basic aiming mechanism.

--- CloneOfTanks/Source/CloneOfTanks/TankPlayerController.cpp
+++ CloneOfTanks/Source/CloneOfTanks/TankPlayerController.cpp
@@ -33,4 +33,67 @@ ATank* ATankPlayerController::GetPlayerTank() const
 	return Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
 }
 
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	AimTowardsCrosshair();
+}
 
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank())
+	{
+		return;
+	}
+
+	FVector HitLocation; // out parameter
+
+	if (GetSightRayHitLocation(HitLocation)) {
+
+	}
+}
+
+bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHitLocation) const
+{
+	FVector LookDirection;
+	if (GetLookDirection(LookDirection))
+	{
+		if (GetLookVectorHitLocation(LookDirection, OutHitLocation)) {
+			UE_LOG(LogTemp, Warning, TEXT("Hitting something at %s"), *OutHitLocation.ToCompactString());
+		}
+
+	}
+	// Line trace along that direction
+
+	// If hit something, return true
+	return false;
+}
+
+bool ATankPlayerController::GetLookDirection(FVector& LookDirection) const
+{
+	// Get Viewport size
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	// Get screen location in pixels
+	FVector2D ScreenLocation(ViewportSizeX * CrosshairXLocation, ViewportSizeY * CrosshairYLocation);
+
+	// De-Project screen position to a world direction
+	FVector WorldLocation;
+	return DeprojectScreenPositionToWorld(ScreenLocation.X, ScreenLocation.Y, WorldLocation, LookDirection);
+}
+
+bool ATankPlayerController::GetLookVectorHitLocation(const FVector &LookDirection, FVector& HitLocation) const
+{
+	FHitResult HitResult;
+	auto StartLocation = PlayerCameraManager->GetCameraLocation();
+	GetWorld()->LineTraceSingleByChannel(HitResult, StartLocation, StartLocation + LookDirection * LineTraceRange, ECollisionChannel::ECC_Visibility);
+
+	if (HitResult.GetActor()) {
+		UE_LOG(LogTemp, Warning, TEXT("Hitting %s"), *HitResult.GetActor()->GetName());
+		HitLocation = HitResult.Location;
+		return true;
+	}
+	else {
+		return false;
+	}
+}
--- CloneOfTanks/Source/CloneOfTanks/TankPlayerController.h
+++ CloneOfTanks/Source/CloneOfTanks/TankPlayerController.h
@@ -21,4 +21,24 @@ private:
 	virtual void BeginPlay() override;
 
 	ATank* GetPlayerTank() const;
+
+	// Moving barrel towards the crosshair
+	void AimTowardsCrosshair();
+
+	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
+	bool GetLookDirection(FVector& LookDirection) const;
+	bool GetLookVectorHitLocation(const FVector &LookDirection, FVector& HitLocation) const;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+	float CrosshairXLocation = 0.5f;
+
+	UPROPERTY(EditAnywhere)
+	float CrosshairYLocation = 0.33333f;
+
+	UPROPERTY(EditAnywhere)
+	float LineTraceRange = 1000000.f; // 10km
 };
