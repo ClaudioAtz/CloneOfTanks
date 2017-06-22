b38aa8a006087ec74d41db65cd30ec8c43ee308c
Claudio <claudio.atz@gmail.com>
2017-06-22 19:51:28 +0100

    Finished basic movement

--- CloneOfTanks/Source/CloneOfTanks/Private/TankMovementComponent.cpp
+++ CloneOfTanks/Source/CloneOfTanks/Private/TankMovementComponent.cpp
@@ -13,10 +13,16 @@ void UTankMovementComponent::IntendMoveForward(float Throw)
 	RightTrack->SetThrottle(Throw);
 }
 
+void UTankMovementComponent::IntendTurnRight(float Throw)
+{
+	if (!LeftTrack || !RightTrack) return;
+
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(-Throw);
+}
+
 void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
 {
-	if (!LeftTrackToSet || !RightTrackToSet) return;
-
 	LeftTrack = LeftTrackToSet;
 	RightTrack = RightTrackToSet;
 }
